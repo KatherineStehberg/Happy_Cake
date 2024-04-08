7d055ad4d6ba3aac35189905a522b3650e243872
Katherine Stehberg <kstehberg@gmail.com>
2024-04-07 22:12:17 -0400

    cambio nombre navigation a nav bar

--- public/src/src/components/Navbar.jsx
+++ public/src/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { Navbar } from "react-bootstrap";
 import "./nav.css";
 
-const Navigation = () => {
+const Navbar = () => {
   return (
     <Navbar className="navbar">
       <Navbar.Brand>

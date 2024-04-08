7cfd182670bdc6711f83267284dbcf323aaa11f6
Katherine Stehberg <kstehberg@gmail.com>
2024-04-07 20:23:01 -0400

    changed main

--- public/src/main.jsx
+++ public/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx"; // Assuming App.jsx contains the entire application setup
+import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
